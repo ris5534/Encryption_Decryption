f6c32d00bb39c47d37a0123df782988e19cb124e
Barakat A. Adediran <46660836+barakat101@users.noreply.github.com>
2019-11-30 17:18:31 -0500

    Update Main.java

--- src/Encryption_Decryption/Main.java
+++ src/Encryption_Decryption/Main.java
@@ -1,25 +1,14 @@
-package Encryption_Decryption;
+//package Encryption_Decryption;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+//import java.io.StringWriter;
 
 public class Main {
     public static final String str = "abcdefghijklmnopqrstuvwxyz";
 
-    public static String encryption(String plaint, int key) {
-        plaint = plaint.toLowerCase();
-        String cipher = "";
-        for (int i = 0; i < plaint.length(); i++) {
-            int charpose = str.indexOf(plaint.charAt(i));
-            int keyval = (charpose + key) % 26;
-            char replaceval = str.charAt(keyval);
-            cipher = cipher + replaceval;
-        }
-        return cipher;
-    }
-
-    public static String encryption2(String plaint, int key) {
+    public static String encrypt(String plaint, int key) {
         plaint = plaint.toLowerCase();
         String cipher = "";
         for (int i = 0; i < plaint.length(); i++) {
@@ -46,17 +35,31 @@ public class Main {
         return palint;
     }
 
+    public static String encrypt2(String plaint) {
+        String cipher = "";
+        plaint = plaint.toLowerCase();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i =0; i < plaint.length(); i++) {
+            cipher = sb.append(plaint.charAt(i) - 'a' + 1).toString();
+        }
+        return cipher;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter your String: ");
         String mag = br.readLine();
         System.out.println("\n");
         System.out.println("Encryption Pattern 1");
-        System.out.println("Your Encrypted String is: " + encryption(mag, 3));
-        System.out.println("Your Decrypted is: " + decrypt(encryption(mag, 3), 3));
+        System.out.println("Your Encrypted String is: " + encrypt(mag, 3));
+        System.out.println("Your Decrypted is: " + decrypt(encrypt(mag, 3), 3));
         System.out.println("\n");
         System.out.println("Encryption Pattern 2");
-        System.out.println("Your Encrypted String is: " + encryption2(mag, 7));
-        System.out.println("Your Decrypted is: " + decrypt(encryption(mag, 7), 7));
+        System.out.println("Your Encrypted String is: " + encrypt2(mag));
+        System.out.println("The Decrypted String should match the original text however we couldn't " +
+                "make the code work.\nYou can however see in the encryption that the letter is replaced " +
+                "with it's number value where A=1 and Z=26");
+
     }
 }
